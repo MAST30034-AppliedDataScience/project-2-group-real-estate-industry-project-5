5c0d51e1ce18d9389088af00a521d2fda14d614d
XINGYOUF <xingyouf@student.unimelb.edu.au>
2024-09-24 16:49:43 +1000

    with shopping center

--- scripts/average.py
+++ scripts/average.py
@@ -3,19 +3,19 @@ from tqdm import tqdm
 
 tqdm.pandas()
 
-file_path = './data/curated/properties_with_sa2_districts.csv'
-data = pd.read_csv(file_path)
+properties_file_path_updated = './data/curated/properties_with_closest_shopping_centers.csv'
+properties_updated_df = pd.read_csv(properties_file_path_updated)
 
-data['Distance (km)'] = pd.to_numeric(data['Distance (km)'], errors='coerce')
-data['School Distance (km)'] = pd.to_numeric(data['School Distance (km)'], errors='coerce')
+properties_updated_df['Price'] = properties_updated_df['cost_text'].str.extract(r'(\d+[\.,]?\d*)').replace(',', '', regex=True).astype(float)
 
-data['Price'] = data['cost_text'].str.extract(r'(\d+[\.,]?\d*)').replace(',', '', regex=True).astype(float)
 
-grouped_avg_data = data.groupby('SA2_NAME21').agg({
-    'Distance (km)': 'mean',
-    'School Distance (km)': 'mean',
-    'Price': 'mean'
+grouped_avg_properties = properties_updated_df.groupby('SA2_NAME21').agg({
+    'Distance (km)': 'mean',  # Distance to station
+    'School Distance (km)': 'mean',  # Distance to school
+    'Distance to Closest Shopping Center (km)': 'mean',  # Distance to shopping center
+    'Price': 'mean'  # Average price
 }).reset_index()
 
-output_file_path = './data/curated/grouped_avg_data_sa2.csv'
-grouped_avg_data.to_csv(output_file_path, index=False)
+
+output_file_path = './data/curated/grouped_avg_properties_sa2.csv'
+grouped_avg_properties.to_csv(output_file_path, index=False)
